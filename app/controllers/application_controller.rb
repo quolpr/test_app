301f25ee1046360c2cd4994b69766ba54b46e968
quolpr <quolpr@gmail.com>
2017-01-29 22:19:18 +0300

    All basics actions for user are implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    %i(sign_up account_update).each do |type|
+      devise_parameter_sanitizer.permit(
+        type, keys: [:email, :name, :password, :password_confirmation]
+      )
+    end
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  email               :string           default(""), not null
+#  encrypted_password  :string           default(""), not null
+#  remember_created_at :datetime
+#  sign_in_count       :integer          default(0), not null
+#  current_sign_in_at  :datetime
+#  last_sign_in_at     :datetime
+#  current_sign_in_ip  :inet
+#  last_sign_in_ip     :inet
+#  name                :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
+end
